ea24339cc810543a565ac184f9ece9f0d3b41797
xl495 <453007797@qq.com>
2024-01-03 23:41:07 +0800

    ✨ feat(add index): add

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import Index from "./pages/Index.vue";
+import { createApp } from "vue";
+
+createApp(Index).mount("body");
---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+console.log("Hello from the popup!");
+</script>
+
+<template>
+  <div class="content">
+    Index
+  </div>
+</template>
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  background-color: aqua;
+}
+
+.content {
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+}
+</style>
